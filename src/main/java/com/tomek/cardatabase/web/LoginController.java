c5fec90418cb9705a3038d76bdf0a969d81ab2a5
Tomasz Czornak <tomasz.czornak59@gmail.com>
2023-02-05 17:06:46 +0100

    bearer {token}- all authentication done

--- src/main/java/com/tomek/cardatabase/web/LoginController.java
+++ src/main/java/com/tomek/cardatabase/web/LoginController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +31,7 @@ public class LoginController {
                 accountCredentials.getUsername(),
                 accountCredentials.getPassword());
 
-        Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+//        Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
         final UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(accountCredentials.getUsername());
         //Generate token
         String jwt = jwtService.getToken(userDetails);
